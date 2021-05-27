cb19db6638c149cc3ee30dc3c90e9d0827e3db42
Sean Russell <seanrussellphotography@gmail.com>
2021-05-26 20:19:21 -0400

    import fontawesome css

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import App from './App';
 
 ReactDOM.render(
