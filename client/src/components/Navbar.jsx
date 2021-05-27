fa74261709ae6e1a93deda3e138f1d2d53a30355
Sean Russell <seanrussellphotography@gmail.com>
2021-05-26 20:21:53 -0400

    NavBar accessible from all protected routes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,36 @@
+import { useHistory } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
+
 // Main search bar, persistent across all routes
 
-export default function Navbar(props) {
+export default function Navbar({ logo }) {
+  let history = useHistory();
+  const goToSearch = () => {
+    history.push('/search')
+  }
+  const goToBookshelf = () => {
+    history.push('/bookshelf')
+  }
+
+  const { logout } = useContext(UserContext)
   return (
-    <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        <img src={props.logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-brand" href="/">
+        <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
         Novella
-      </a>
+      </div>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <button className="nav-link btn btn-outline-link my-2 my-sm-0" onClick={goToBookshelf}>My Bookshelf</button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link btn btn-outline-link my-2 my-sm-0" onClick={goToSearch}>Search</button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link btn btn-outline-link my-2 my-sm-0" onClick={logout}>Logout</button>
+        </li>
+      </ul>
     </nav>
   )
 }
