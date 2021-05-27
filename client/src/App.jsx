07decffa8ac25288e05161d1320a759f79d6e0fe
Sean Russell <seanrussellphotography@gmail.com>
2021-05-26 20:12:43 -0400

    context providers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import { UserContextProvider } from './contexts/UserContext'
 import AppRouter from './routes/AppRouter';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BooksContextProvider } from './contexts/BooksContext';
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <UserContextProvider>
-      <AppRouter />
-    </UserContextProvider>
+    // Wrapping router with contexts here so I don't have to do it later
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>
+        <BooksContextProvider>
+
+          <AppRouter />
+
+        </BooksContextProvider>
+      </UserContextProvider>
+    </QueryClientProvider>
   );
 }
