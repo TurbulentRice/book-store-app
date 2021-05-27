f512e37de081a3a1ddf960cf171b5aa96894ac0a
Sean Russell <seanrussellphotography@gmail.com>
2021-05-26 21:07:10 -0400

    copy paste bootstrap css to styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './styles/bootstrap.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import App from './components/App';
 
