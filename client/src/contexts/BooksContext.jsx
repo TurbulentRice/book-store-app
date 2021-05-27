2b9673419227675e4ca2386f077943df412b6a28
Sean Russell <seanrussellphotography@gmail.com>
2021-05-26 20:23:20 -0400

    added updater functions to BooksContext, likely won't need this anyway...

--- client/src/contexts/BooksContext.jsx
+++ client/src/contexts/BooksContext.jsx
@@ -8,17 +8,21 @@ export const BooksContext = createContext();
 
 export function BooksContextProvider({ children }) {
   
-  // bookBucket will store uncategorized books
-  const [bookBucket, setBookBucket] = useState([]);
+  // bookShelf will store uncategorized books
+  const [bookShelf, setBookShelf] = useState([]);
   // The rest will be distributed to user's shelves
   const [wantToRead, setWantToRead] = useState([]);
   const [currentlyReading, setCurrentlyReading] = useState([]);
   const [alreadyRead, setAlreadyRead] = useState([]);
 
-  const addWantToRead = (book) => setWantToRead([...wantToRead, book])
+  const addWantToRead = book => setWantToRead([...wantToRead, book])
+  const addCurrentlyReading = book => setCurrentlyReading([...currentlyReading, book])
+  const addAlreadyRead = book => setAlreadyRead([...alreadyRead, book])
+
+  const states = {bookShelf, setBookShelf, wantToRead, currentlyReading, alreadyRead, addWantToRead, addCurrentlyReading, addAlreadyRead}
 
   return (
-    <BooksContext.Provider value={{}}>
+    <BooksContext.Provider value={states}>
       {children}
     </BooksContext.Provider>
   );
