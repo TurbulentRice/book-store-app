7feaaace66646d579e32588066e4fec91be3bf48
Sean Russell <seanrussellphotography@gmail.com>
2021-05-27 01:01:32 -0400

    moved books context to user context - nice

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -1,21 +1,26 @@
 import { createContext, useState } from "react";
 
   /**
-   * User auth context
+   * User context for auth and session history
    */
 
 export const UserContext = createContext();
 
 export function UserContextProvider({ children }) {
-  
+  // Token
   const [token, setToken] = useState("");
-
   const isLoggedIn = () => !!token;
   const login = (token) => setToken(token);
   const logout = () => setToken("");
 
+  // Searches and things to keep handy in browser, but not on server
+  const [searchString, setSearchString] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const states = {token, isLoggedIn, login, logout, searchResults, setSearchResults, searchString, setSearchString}
+
   return (
-    <UserContext.Provider value={{token, isLoggedIn, login, logout}}>
+    <UserContext.Provider value={states}>
       {children}
     </UserContext.Provider>
   );
